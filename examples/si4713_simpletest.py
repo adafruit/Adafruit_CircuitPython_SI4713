d74985de7305814197ed4fb7e13727d90b949e82
dherrada <dylan.herrada@adafruit.com>
2021-01-13 13:02:49 -0500

    Ran pre-commit, added licenses

--- examples/si4713_simpletest.py
+++ examples/si4713_simpletest.py
@@ -1,3 +1,6 @@
+# SPDX-FileCopyrightText: 2021 ladyada for Adafruit Industries
+# SPDX-License-Identifier: MIT
+
 # Simple demo of using the SI4743 RDS FM transmitter.
 # Author: Tony DiCola
 import time
